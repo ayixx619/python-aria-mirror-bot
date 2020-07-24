7ea670ea3c7e88d0448fcb87c08476ec9ee14798
AyiXX <53760265+ayixx619@users.noreply.github.com>
2020-07-24 23:38:28 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./aria.sh; python3 -m bot
+worker:curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',' | cat && export tracker="[$tracker_list]" ./aria.sh && python3 -m bot
